89d19db866b6f4f59ded0e2488798a559d809801
Olga <olga.kuratkina@gmail.com>
2017-01-29 20:13:17 +0200

    edited fixture, stable version

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,17 +1,7 @@
 # -*- coding: utf-8 -*-
-import pytest
-
-from fixture.application import Application
 from model.contact import Contact
 
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
-    
 def test_test_add_contact(app):
     app.session.login("admin", "secret")
     app.contact.add_contact(Contact(name="Olga", second_name="J", last_name="Kuratkina", nickname="BlackCat", position="AQA", company="LvivIT", address="Lviv", phone="0979247672",
