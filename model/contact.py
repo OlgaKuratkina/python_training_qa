4c92a5586d88cbcebba92c94bd4d93644e98e0da
Olga <olga.kuratkina@gmail.com>
2017-02-13 19:55:42 +0200

    added comparison for last name

--- model/contact.py
+++ model/contact.py
@@ -16,7 +16,7 @@ class Contact:
         self.id = id
 
     def __eq__(self, other):
-        return (self.id == other.id or self.id is None or other.id is None) and (self.name == other.name)
+        return (self.id == other.id or self.id is None or other.id is None) and (self.name == other.name) and (self.last_name == other.last_name)
 
     def __repr__(self):
         return "Contact Id: %s, Name: %s, Last Name: %s" % (self.id, self.name, self.last_name)
